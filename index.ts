70cb0eec87e647fba51e897e0689a3886ef40786
t33y <omotayoolarewaju@gmail.com>
2023-10-23 18:55:48 +0100

    first commit

---
+++ index.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response } from "express";
+import { Resend } from "resend";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const corsOptions = {
+  origin: "*",
+};
+app.use(cors(corsOptions));
+
+app.post("/", async (req: Request, res: Response) => {
+  try {
+    const data = await resend.emails.send({
+      from: "Portfolio <onboarding@resend.dev>",
+      to: ["omotayoolarewaju@gmail.com"],
+      subject: "Message From Contact Form",
+      html: `<strong>${req.body.message}</strong>`,
+      reply_to: req.body.email,
+    });
+    console.log("body", req.body);
+    console.log("body", res.statusCode);
+    res.status(200).json({ data });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+app.listen(3001, () => {
+  console.log("not listening", process.env.RESEND_API_KEY);
+  console.log("Listening on http://localhost:3001");
+});
