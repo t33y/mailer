fb788c5e98600d0ed756bedb2435cde46a61cc82
t33y <omotayoolarewaju@gmail.com>
2023-10-24 00:47:40 +0100

    removes app.listen

--- api/index.ts
+++ api/index.ts
@@ -33,6 +33,6 @@ app.post("/", async (req: Request, res: Response) => {
 
 export default app;
 
-app.listen(3001, () => {
-  console.log("Listening on http://localhost:3001");
-});
+// app.listen(3001, () => {
+//   console.log("Listening on http://localhost:3001");
+// });
